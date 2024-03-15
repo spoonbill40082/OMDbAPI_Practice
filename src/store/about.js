4efe4ef1f008624baad68bf95de64d97a9ce1b4b
HyeJeong <hj400820908@gmail.com>
2024-03-15 12:58:01 +0900

    firt commit

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+import { Store } from "../core/heropy";
+
+//여기서 정보를 바꿔주면 다른 페이지에 한 번에 반영된다.
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'HEROPY / ParkYoungWoong',
+  email: 'thesecon@gmail.com',
+  blog: 'https://heropy.blog',
+  github: 'https://github.com/ParkYoungWoong',
+  repository: 'https://github.com/ParkYoungWoong/vanillajs-movie-app'
+})
---
+++ src/store/movie.js
@@ -0,0 +1,48 @@
+import { Store } from "../core/heropy";
+
+const store = new Store({
+  searchText: '',
+  page: 1,
+  pageMax: 1,
+  movies: [],
+  movie: {},
+  loading: false,
+  message: 'Search for the Movie Title'
+})
+
+export default store
+export const searchMovies = async page => {
+  store.state.loading = true
+  store.state.page = page
+  if (page === 1) {
+    store.state.movies = []
+    store.state.message = ''
+  }
+  try {
+    const res = await fetch(`https://omdbapi.com/?apikey=3c83997e&s=${store.state.searchText}&page=${page}`)
+    const { Search, totalResults, Response, Error } = await res.json()
+    if (Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies,
+        ...Search
+      ]
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+    } else {
+      store.state.message = Error
+      store.state.pageMax = 1
+    }
+  } catch (error) {
+    console.log('searchMovies error:', error)
+  } finally {
+    store.state.loading = false
+  }
+}
+
+export const getMovieDetails = async id => {
+  try {
+    const res = await fetch(`https://omdbapi.com/?apikey=3c83997e&i=${id}&plot=full`)
+    store.state.movie = await res.json()
+  } catch (error) {
+    console.log('getMovieDetails error:', error)
+  }
+} 
